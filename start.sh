eb2429dff20c6b318893dc09991b9af189f66c9e
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-08-24 17:01:54 +0200

    make sure startup script loads database to mongo

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/bin/bash -c "mongod --fork --logpath /var/log/mongod.log --dbpath /root/mongo"
+/bin/bash -c "mongod --fork --logpath /var/log/mongod.log --dbpath /root/mongo && cd /root && mongorestore"
 source /root/catkin_ws/devel/setup.bash
 exec "$@"
