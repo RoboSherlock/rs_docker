f264d6bf685b7ba0414cffcbc808746de227e998
Franklin <fkenghag@uni-bremen.de>
2019-09-12 13:55:57 +0200

    Integration of commands and ports for web visualizer

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 IMAGE_NAME=robosherlock/rs_interactive
 
 docker run -d \
-  -p 3000:3000 -p 1111:5555 -p 9090:9090 -p 8080:8080 \
+  -p 3000:3000 -p 1111:5555 -p 9090:9090 -p 8080:8080 -p 8081:8081 \
   --name rs_container \
 ${IMAGE_NAME}
